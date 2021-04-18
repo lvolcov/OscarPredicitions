d9b6b8f71f63e0dd8f9b0e26c34ef7c10494284b
Lucas Volcov <lucas.volcov@hotmail.com>
2021-04-18 17:32:25 +0100

    commit message

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // http://49.213.81.43/static/tool/thuocbot/node_modules/telegraf/docs/#/
 
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.BOT_TOKEN)
+// const { Telegraf } = require('telegraf')
+// const bot = new Telegraf(process.env.BOT_TOKEN)
 
-// const { Composer } = require('micro-bot')
-// const bot = new Composer
+const { Composer } = require('micro-bot')
+const bot = new Composer
 
 
 const answers = require('./functions/answers')
@@ -73,5 +73,5 @@ bot.use(async (ctx) => {
     }
 })
 
-// module.exports = bot
-bot.launch()
+module.exports = bot
+// bot.launch()
